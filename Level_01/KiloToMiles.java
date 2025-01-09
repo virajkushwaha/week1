1dfe72adeea40dd012e9feaf73db9c148c0e337a
virajkushwaha <virajkushwaha21@gmail.com>
2025-01-09 16:00:10 +0530

    KiloToMiles

---
+++ Level_01/KiloToMiles.java
@@ -0,0 +1,20 @@
+// Create a class KiloToMiles for converting the distance kilometer to miles 
+
+public class KiloToMiles{
+	
+	public static void main(String args[]){
+		
+		//Create the variable for kiloMeter
+		float kiloMeter = 10.8f;
+		
+		//Create the variable for miles
+		float miles = kiloMeter / 1.6f;
+		
+		
+		
+		//Print the converted value kilometre to miles 
+		System.out.println(" The distance "+ kiloMeter +" km in miles is "+miles);
+		
+		
+	}
+}
