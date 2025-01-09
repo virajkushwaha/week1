51d679ba8c64506c6e8318aadc1c1124a060b3af
virajkushwaha <virajkushwaha21@gmail.com>
2025-01-09 15:53:18 +0530

    HarryAgeCalculator

---
+++ Level_01/HarryAgeCalculator.java
@@ -0,0 +1,21 @@
+// Create a class name same as filename HarryAgeCalculator
+
+public class HarryAgeCalculator{
+	
+	public static void main(String args[]){
+		
+		//Create the variable for birth year of Harry 
+		int birthYear = 2000;
+		
+		//Create the variable for present year
+		int presentYear = 2024;
+		
+		//Create the variable for age 
+		int presentAge = presentYear - birthYear;
+		
+		//Print the Age of Harry as of presentYear
+		System.out.println(" Harry's age in 2024 is "+presentAge);
+		
+		
+	}
+}
