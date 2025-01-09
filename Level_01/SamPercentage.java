c5acb3a5a973d57e4565ee09f4928f6fbb55279b
virajkushwaha <virajkushwaha21@gmail.com>
2025-01-09 15:58:51 +0530

    SamPercentage

---
+++ Level_01/SamPercentage.java
@@ -0,0 +1,24 @@
+// Create a class SamPercentage for calculating the average percent
+
+public class SamPercentage{
+	
+	public static void main(String args[]){
+		
+		//Create the variable for math marks 
+		int maths = 94;
+		
+		//Create the variable for physics marks
+		int physics = 95;
+		
+		//Create the variable for chemistry marks 
+		int chemistry = 96;
+		
+		//Create the variable for  average percent
+		float percentage = (maths+physics+chemistry)/3.0f;
+		
+		//Print the Average percent of Sam
+		System.out.println("Sam's average mark in PCM is "+percentage);
+		
+		
+	}
+}
